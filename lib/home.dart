8e6d31ce6a2cd71c92ac708abe6f6827d383bde8
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-02-06 17:42:05 +0700

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constant/colors.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: tdBGColor,
+      // appBar: ,
+    );
+  }
+
+  // App bar
+  // AppBar _buildApp() {
+
+  // }
+}
