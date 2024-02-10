a27fa82dc7049917de91f6efef48fe9e69273825
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-02-10 07:56:57 +0700

    Todo Items done, just have to put that at home

--- lib/widget/todo_item.dart
+++ lib/widget/todo_item.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/model/ToDo.dart';
 import '../constant/colors.dart';
 
 class ToDoItem extends StatelessWidget {
-  const ToDoItem({super.key});
+  const ToDoItem({
+    super.key,
+    required this.todo,
+    required this.onToDoChanged,
+    required this.onDeleteItem,
+  });
+  final ToDo todo;
+  final onToDoChanged;
+  final onDeleteItem;
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +24,35 @@ class ToDoItem extends StatelessWidget {
         ),
         contentPadding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
         tileColor: Colors.white,
-        // leading: Icon(
-        //   todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
-        //   color: tdBlue,
-        // ),
+        leading: Icon(
+          todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+          color: tdBlue,
+        ),
+        title: Text(
+          todo.todoText!,
+          style: TextStyle(
+            fontSize: 16,
+            color: tdBlack,
+            decoration: todo.isDone
+                ? TextDecoration.lineThrough
+                : null, // apabbila true maka akan ada tambahan garis melintang
+          ),
+        ),
+        trailing: Container(
+          padding: const EdgeInsets.all(0),
+          margin: const EdgeInsets.symmetric(vertical: 12),
+          width: 35,
+          height: 35,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: tdRed,
+          ),
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.delete),
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
